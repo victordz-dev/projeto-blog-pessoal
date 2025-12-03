fff7a46ce13d1dbcd570d34aaff3b1d0fa440297
Victor Rodriguez <164812973+victordz-dev@users.noreply.github.com>
2025-12-03 10:37:54 -0300

    adicionando métodos crud

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,6 +1,18 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { Postagem } from '../entities/postagem.entity';
 import { PostagemService } from '../services/postagem.service';
+import { DeleteResult } from 'typeorm';
 
 @Controller('/postagens')
 export class PostagemController {
@@ -9,5 +21,35 @@ export class PostagemController {
   @HttpCode(HttpStatus.OK)
   findAll(): Promise<Postagem[]> {
     return this.postagemService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+    return this.postagemService.findByID(id);
+  }
+
+  @Get('/titulo/:title')
+  @HttpCode(HttpStatus.OK)
+  findByTitle(@Param('title') title: string): Promise<Postagem[]> {
+    return this.postagemService.findByTitle(title);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.create(postagem);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.update(postagem);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+    return this.postagemService.delete(id);
   }
 }
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { ILike, Repository } from 'typeorm';
 import { Postagem } from '../entities/postagem.entity';
+import { DeleteResult } from 'typeorm/browser';
 
 @Injectable()
 export class PostagemService {
@@ -11,5 +12,43 @@ export class PostagemService {
   ) {}
   async findAll(): Promise<Postagem[]> {
     return this.postagemRepository.find();
+  }
+  async findByID(id: number): Promise<Postagem> {
+    const postagem = await this.postagemRepository.findOne({
+      where: { id },
+    });
+    if (!postagem) {
+      throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+    }
+    return postagem;
+  }
+  async findByTitle(title: string): Promise<Postagem[]> {
+    return await this.postagemRepository.find({
+      where: {
+        titulo: ILike(`%${title}%`),
+      },
+    });
+  }
+
+  async create(postagem: Postagem): Promise<Postagem> {
+    return this.postagemRepository.save(postagem);
+  }
+
+  async update(postagem: Postagem): Promise<Postagem> {
+    const postAtualizada = postagem.id;
+
+    if (!postAtualizada) {
+      throw new HttpException(
+        'Postagem não encontrada',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return this.postagemRepository.save(postagem);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findByID(id);
+
+    return await this.postagemRepository.delete(id);
   }
 }
