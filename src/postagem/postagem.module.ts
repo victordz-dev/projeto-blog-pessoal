5b4f34a42335e634370bda789eb152b2293fab6d
Victor Rodriguez <164812973+victordz-dev@users.noreply.github.com>
2025-12-15 20:22:35 -0300

    fix: arrumando exportação e importação de modulos

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -4,11 +4,10 @@ import { PostagemController } from './controllers/postagem.controller';
 import { Postagem } from './entities/postagem.entity';
 import { PostagemService } from './services/postagem.service';
 import { TemaModule } from '../tema/tema.module';
-import { TemaService } from '../tema/services/tema.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Postagem]), TemaModule],
-  providers: [PostagemService, TemaService],
+  providers: [PostagemService],
   controllers: [PostagemController],
   exports: [TypeOrmModule],
 })
